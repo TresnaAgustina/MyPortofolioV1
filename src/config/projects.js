b6db85ceaca7d206019535508496eedfcd7ce3b6
TresnaAgustina <tresnaa001@gmail.com>
2023-12-28 16:40:31 +0800

    adding new project

--- src/config/projects.js
+++ src/config/projects.js
@@ -1,22 +1,22 @@
 export const project = [
   {
-    title: "Personal Portfolio",
+    title: "Digitalisasi Lontar",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-    image: "/assets/images/Project1.png",
-    link: "https://www.google.com",
+    image: "/assets/images/ProjectLontar.svg",
+    link: "https://github.com/TresnaAgustina/LontarDigital",
     backgroundColor: "bg-gradient-to-br from-black_0 to-black_1",
-    year: "2020",
-    category: "UI Design",
+    year: "2023",
+    category: "Web App Develop",
   },
   {
     title: "Tresna Portofolio UI",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
     image: "/assets/images/Project2.png",
-    link: "https://www.google.com",
+    link: "https://www.notion.so/tresna/My-Portfolio-V-1-b6e8500455264303bd850eeaa6736586?pvs=4",
     backgroundColor: "bg-gradient-to-br from-black_0 to-black_1",
-    year: "2020",
+    year: "2023",
     category: "UI Design & Development",
   },
   {
@@ -26,7 +26,7 @@ export const project = [
     image: "/assets/images/Project3.png",
     link: "https://www.notion.so/tresna/Saguna-Sigma-UI-Design-b32e921c04154badbebda7abb8f752e8?pvs=4",
     backgroundColor: "bg-gradient-to-br from-black_0 to-black_1",
-    year: "2020",
+    year: "2022",
     category: "UI Design & Development",
   },
 ];
